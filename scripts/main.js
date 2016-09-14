7a0f24cc4cce7c564cc4cda99df6cd4f66f45970
Rachelle <archaurore@gmail.com>
2016-09-13 23:05:51 -0400

    added ajax call to Wiki API

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,30 @@
 //Date: Sept, 2016
 
 $(document).ready(function() {
+  var search = "";//write this
+  var wikipURL = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&rvprop=content&titles=";
+  wikipURL += search + "&callback=?";;
 
-  
+  $('#submit').on('click', function(e) {
+    e.preventDefault;
+    $('.result-field').empty();
+    getData();
+
+  });
+
+  function getData(wikipURL) {
+    $.getJSON(wikipURL).done(function(data) {
+      var result = data.query.pages;
+      var numberOfPages = Object.keys(data).length;
+      var arr = [result, numberOfPages];
+      return arr;
+    }).fail(function(err) {
+      $('.result-field').append("<p>Something has gone wrong! " + err + "</p>");
+    });
+  }
+
+  function showArticles() {
+//write this
+  }
+
 });
