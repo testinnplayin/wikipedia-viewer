695dbbf4333a15da29aa88bccd06d44a3b0503f1
Rachelle <archaurore@gmail.com>
2016-09-14 21:49:23 -0400

    added keydown event in search field

--- scripts/main.js
+++ scripts/main.js
@@ -3,30 +3,52 @@
 //Date: Sept, 2016
 
 $(document).ready(function() {
-  var search = "";//write this
-  var wikipURL = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&rvprop=content&titles=";
-  wikipURL += search + "&callback=?";;
 
-  $('#submit').on('click', function(e) {
-    e.preventDefault;
-    $('.result-field').empty();
-    getData();
+  function getValue() {
+    var search = $('#search-input').val();
+    var wikipURL = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=&list=search&srsearch=";
+    wikipURL += search + "&srlimit=8&callback=?";
+    return wikipURL;
+  }
 
-  });
+  function showArticle(title, snippet) {
+    var $article = $("<a href='https://en.wikipedia.org/wiki/" + title
+      + "' class='list-group-item' target='_blank'><i class='fa fa-newspaper-o'></i><strong> " + title + "</strong>: "
+      + snippet + "</a>");
+    $('.result-field').append($article);
+  }
 
-  function getData(wikipURL) {
-    $.getJSON(wikipURL).done(function(data) {
-      var result = data.query.pages;
-      var numberOfPages = Object.keys(data).length;
-      var arr = [result, numberOfPages];
-      return arr;
+  function getData(url) {
+    $.getJSON(url).done(function(data) {
+      var title, snippet;
+      for (var i = 0; i < 8; i++) {
+        title = data.query.search[i].title;
+        snippet = data.query.search[i].snippet;
+        showArticle(title, snippet);
+      }
     }).fail(function(err) {
       $('.result-field').append("<p>Something has gone wrong! " + err + "</p>");
     });
   }
 
-  function showArticles() {
-//write this
+  function doActions(e) {
+    e.preventDefault();
+    $('.result-field').empty();
+    var url = getValue();
+    getData(url);
   }
+
+  $('#submit').on('click', function(e) {
+    doActions(e);
+  });
+
+  $('#search-input').keydown(function(e) {
+
+    if (e.which == 13) {
+      doActions(e);
+    }
+  });
+
+
 
 });
